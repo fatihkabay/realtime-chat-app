5cb3724db4a020d1714a0a8890bc84859cc66f5f
Fatih Kabay <mfthkabay@gmail.com>
2023-11-16 23:17:49 +0300

    write user model and hash password

--- server/index.js
+++ server/index.js
@@ -1,25 +1,27 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/userRoutes");
 const app = express();
 require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URL, {
+app.use("/api/auth", userRoutes);
+
+mongoose
+  .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-})
-.then(() => {
+  })
+  .then(() => {
     console.log("DB Connection Successfull");
-})
-.catch((err) => {
+  })
+  .catch((err) => {
     console.log(err.message);
-})
-;
+  });
 
 const server = app.listen(process.env.PORT, () => {
-    console.log(`Server started on port ${process.env.PORT}!`);
+  console.log(`Server started on port ${process.env.PORT}!`);
 });
