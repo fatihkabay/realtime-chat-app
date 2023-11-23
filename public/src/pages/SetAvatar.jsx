a200cf67123cd6472920f203a181c1bf5173cbf8
Fatih Kabay <mfthkabay@gmail.com>
2023-11-24 00:03:27 +0300

    design page

--- public/src/pages/SetAvatar.jsx
+++ public/src/pages/SetAvatar.jsx
@@ -6,6 +6,7 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import axios from "axios";
 import { setAvatarRoute } from "../utils/APIRoutes";
+import { Buffer } from "buffer";
 
 export default function SetAvatar() {
   const api = "https://api.multiavatar.com/45678945";
@@ -26,13 +27,11 @@ export default function SetAvatar() {
 
   useEffect(() => {
     const data = [];
-    for (let i=0; i<4; i++) {
-        const image = await axios.get(
-            `${api}/${Math.round(Math.random() * 1000)}`
-        );
-        const buffer = new Buffer(image.data);
-        data.push(buffer.toString("base64"));
-    };
+    for (let i = 0; i < 4; i++) {
+      const image = axios.get(`${api}/${Math.round(Math.random() * 1000)}`);
+      const buffer = new Buffer.alloc(image.data);
+      data.push(buffer.toString("base64"));
+    }
     setAvatars(data);
     setIsLoading(false);
   }, []);
@@ -43,19 +42,82 @@ export default function SetAvatar() {
         <div className="title-container">
           <h1>Pick an avatar as your profile picture</h1>
         </div>
-        <div className="avatars">{
-            avatars.map((avatars, index) => {
-                return (
-                    <div className={`avatar ${selectedAvatars === index ? "selected" : ""}`}>
-                        <img src={`data:image/svg+xml; base64, ${avatars}`} alt="avatar" onClick={() => setSelectedAvatar(index)}/>
-                    </div>
-                )
-            })
-        }</div>
+        <div className="avatars">
+          {avatars.map((avatars, index) => {
+            return (
+              <div
+                className={`avatar ${
+                  selectedAvatars === index ? "selected" : ""
+                }`}
+                key={index}
+              >
+                <img
+                  src={`data:image/svg+xml; base64, ${avatars}`}
+                  alt="avatar"
+                  onClick={() => setSelectedAvatars(index)}
+                />
+              </div>
+            );
+          })}
+        </div>
       </Container>
       <ToastContainer />
     </>
   );
 }
 
-const Container = styled.div``;
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 3rem;
+  background-color: #131324;
+  height: 100vh;
+  width: 100vw;
+
+  .loader {
+    max-inline-size: 100%;
+  }
+
+  .title-container {
+    h1 {
+      color: white;
+    }
+  }
+  .avatars {
+    display: flex;
+    gap: 2rem;
+
+    .avatar {
+      border: 0.4rem solid transparent;
+      padding: 0.4rem;
+      border-radius: 5rem;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      transition: 0.5s ease-in-out;
+      img {
+        height: 6rem;
+        transition: 0.5s ease-in-out;
+      }
+    }
+    .selected {
+      border: 0.4rem solid #4e0eff;
+    }
+  }
+  .submit-btn {
+    background-color: #4e0eff;
+    color: white;
+    padding: 1rem 2rem;
+    border: none;
+    font-weight: bold;
+    cursor: pointer;
+    border-radius: 0.4rem;
+    font-size: 1rem;
+    text-transform: uppercase;
+    &:hover {
+      background-color: #4e0eff;
+    }
+  }
+`;
