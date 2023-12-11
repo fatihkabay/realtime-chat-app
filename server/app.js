0a89f4b543aa6a3e568a38dee57e3db343a2e648
Fatih Kabay <mfthkabay@gmail.com>
2023-12-11 23:42:00 +0300

    db insert

--- server/app.js
+++ server/app.js
@@ -1,28 +1,42 @@
-const express = require('express');
-const { connectToDb, getDb } = require('./index')
+const express = require("express");
+const { connectToDb, getDb } = require("./index");
 
 const app = express();
 
 let db;
 
 connectToDb((err) => {
-    if (!err) {
-        app.listen(3000, () => {
-            console.log('app listening on port 3000');
-        })
-        db = getDb();
-    }
-})
+  if (!err) {
+    app.listen(3000, () => {
+      console.log("app listening on port 3000");
+    });
+    db = getDb();
+  }
+});
 
-app.get('/api', (req, res) => {
-    let users = [];
-    db.collection('user')
-       .find()
-       .sort({username: -1})
-       .forEach(user=> users.push(user))
-       .then(() => {
-        res.status(200).json(users)
-       }).catch(() => {
-        res.status(500).json({hata: 'Not found data'})
-       })
-})
+app.get("/api", (req, res) => {
+  let user = [];
+  db.collection("user")
+    .find()
+    .sort({ username: -1 })
+    .forEach((users) => user.push(users))
+    .then(() => {
+      res.status(200).json(user);
+    })
+    .catch(() => {
+      res.status(500).json({ hata: "Not found data" });
+    });
+});
+
+app.post("api", (req, res) => {
+  const user = req.body;
+
+  db.collection("user")
+    .insertOne()
+    .then((sonuc) => {
+      res.status(201).json(sonuc);
+    })
+    .catch((err) => {
+      res.status(500).json({ hata: "Did not add data" });
+    });
+});
